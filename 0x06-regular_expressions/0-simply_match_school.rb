18b8b4a5321305813d06b7625f0fba612ea7abc4
oladapsy <dapoodedeyi@yahoo.com>
2024-02-27 16:49:23 -0800

    test

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+#since we only want a particular word with case sensitive i only used the g
+#flag for global
+#by: odedeyi oladapo olayinka
+
+/Holberton/g
---
+++ 0x06-regular_expressions/README.md
@@ -0,0 +1,2 @@
+0x06-regular_expressions
+This task was done on my mobile phonee
