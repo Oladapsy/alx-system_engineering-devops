706e966c84e8b61fcd3ceacc3306bf6b471cf1aa
Oladapsy <dapoodedeyi@yahoo.com>
2024-04-24 19:04:28 +0100

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,12 @@
 # Using Puppet, install flask from pip3.
+package { 'python3.8':
+    ensure  => '3.8.10'
+}
+
+package { 'werkzeug':
+  ensure   => 'latest',
+  provider => 'pip3',
+}
 
 package { 'flask':
   ensure   => '2.1.0',
