f002432d70dc0cd92e52c1fe30e9c53a343da5bd
Oladapsy <dapoodedeyi@yahoo.com>
2024-04-24 19:28:16 +0100

    test

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,16 @@
 # Using Puppet, install flask from pip3.
 package { 'python3.8':
-    ensure => '3.8.10',
+  ensure   => '3.8.10',
+  provider => 'pip3', 
 }
 
-package { 'flask':
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
 
-package { 'werkzeug':
+#instaling werkzeug as a requirement for flask
+package { 'Werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
   require  => Package['Flask'],
