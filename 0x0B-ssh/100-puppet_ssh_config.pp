93edf11c1e0e73fe19e8e3f432c2abc44ad28435
Oladapsy <dapoodedeyi@yahoo.com>
2024-05-04 23:15:20 +0100

    test_it

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 # Client configuration file (w/ Puppet)
+file {'/etc/ssh/shh_config':
+	ensure  => 'present',
+}
+
 file_line {'Turn off passwd auth':
-	ensure => 'present',
-	path   => '/etc/ssh/ssh_config'
-	line   => 'PasswordAuthentication no',
-	match  => 'PasswordAuthentication yes',
+	path    => '/etc/ssh/ssh_config',
+	line    => 'PasswordAuthentication no',
+	match   => 'PasswordAuthentication yes',
+	replace => 'true',
+	
 }
 file_line {'Declare identity file':
-	ensure => 'present',
-	path   => '/etc/ssh/ssh_config',
-	line   => 'IdentityFile ~/.ssh/school',
+	path    => '/etc/ssh/ssh_config',
+	line    => 'IdentityFile ~/.ssh/school',
+	match   => '^IdentityFile',
+	ensure  => 'present',
 }
