981fd594bd36189794c3ba8dcd184c3fe795a051
Oladapsy <dapoodedeyi@yahoo.com>
2024-05-06 18:31:45 +0100

    try_it_smew

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,42 +1,27 @@
-# Install Nginx web server (w/ Puppet)
-# To save time and effort, you should also include resources in your manifest to perform a 301 redirect when querying /redirect_me
-# Nginx should be listening on port 80
- When querying Nginx at its root / with a GET request (requesting a page) using curl, it must return a page that contains the string Hello World!
-# The redirection must be a “301 Moved Permanently”
-# Your answer file should be a Puppet manifest containing commands to automatically configure an Ubuntu machine to respect above requirements
+#Time to practice configuring your server with Puppet! Just as 
+#you did before, we’d like you to install and configure an Nginx
+#server using Puppet instead of Bash. To save time and effort, you
+#should also include resources in your manifest to perform a 301
+#redirect when querying /redirect_me.
 
-# install nginx with puppet
 
+# Install Nginx with puppet
 package { 'nginx':
-	ensure => installed,
+  ensure => installed,
 }
 
 file_line { 'install':
-	ensure => 'present',
-	path   => '/etc/nginx/sites-available/default',
-	after  => 'listen 80 default_server;',
-	line   => 'rewrite ^/redirect_me https://www.youtube.com/@cryptotechcoder permanent;',
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-file { '/var/www/html':
-	ensure => 'directory',
-}
-
-file { '/var/www/html/index.html',
-	content => 'Hello World!',
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
 }
 
 service { 'nginx':
-	ensure  => running,
-	require => package['nginx'],
-}
-
-file { '/etc/nginx/sites-enabled/default':
-	ensure  => 'link',
-	target  => '../sites-available/default',
-}
-service { 'nginx':
-  	ensure  => running,
-  	require => package['nginx'],
-  	notify  => Service['nginx'],
+  ensure  => running,
+  require => Package['nginx'],
 }
