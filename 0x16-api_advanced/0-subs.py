386c7f3b8fd96c78da6def438ac06b24e7986d16
oladapsy <dapoodedeyi@yahoo.com>
2024-08-14 03:18:36 +0100

    refactor

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ queries the Reddit API and returns the number of subscribers """
 import requests
-"""the request model"""
 
 
 def number_of_subscribers(subreddit):
@@ -9,14 +8,10 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about/.json"
     headers = {'User-Agent': 'my-app/0.0.1'}
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            all_r = response.json()
-            data = all_r.get('data')
-            sub_count = data.get('subscribers')
-            return sub_count
-        else:
-            return 0
-    except requests.RequestException:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    all_r = response.json()
+    data = all_r.get('data')
+    sub_count = data.get('subscribers')
+    return sub_count
