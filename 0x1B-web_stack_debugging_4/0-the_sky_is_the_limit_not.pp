78522836caccfe47542c49bb1ee69ceebce2e910
oladapsy <dapoodedeyi@yahoo.com>
2024-08-22 10:38:06 +0100

    fix the nginx user limit in the default file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,8 @@
 # increase the user limit of nginx server to be able to serve to more traffic
 
-exec { 'Nginx-User-Limit-Increase':
-    command => 'sed -i \'s/ULIMIT="-n 15" /ULIMIT="-n 4096"/\' /etc/default/nginx',
+#increase user limit of default
+exec { 'nginx-user-limit-increase':
+    command => 'sed -i \'s/ULIMIT="-n 15"/ULIMIT="-n 4096"/\' /etc/default/nginx',
     path    => '/bin/:/usr/bin/',
 }
 exec { 'restart-nginx':
